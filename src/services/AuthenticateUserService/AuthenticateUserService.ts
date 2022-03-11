e7647774e8a797beed5a52cafbfcc63c16686b25
ReneSena <rene.sena3005@gmail.com>
2022-03-10 21:31:43 -0300

    Creating an interface to user data

--- src/services/AuthenticateUserService/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService/AuthenticateUserService.ts
@@ -5,14 +5,21 @@ import axios from 'axios';
  * Recover the access_token on github
  * Recover the user data
  * Verify if the user exist on DB
- * ---Yes = Generate a Token 
+ * ---Yes = Generate a Token
  * ---No = Create user on DB, Generate a Token
- * 
+ *
  * -> Return the Token with user infos <-
  */
 
 interface IAcessTokenResponse {
     access_token: string;
+}
+
+interface IUserResponse {
+	avatar_url: string;
+	login: string;
+	id: number;
+	name: string;
 }
 
 class AuthenticateUserService {
@@ -24,13 +31,13 @@ class AuthenticateUserService {
                 client_id: process.env.GITHUB_CLIENT_ID,
                 client_secret: process.env.GITHUB_CLIENT_SECRET,
                 code,
-            }, 
+            },
             headers: {
                 "Accept": "application/json"
             }
         });
 
-        const response = await axios.get('https://api.github.com/user', {
+        const response = await axios.get<IUserResponse>('https://api.github.com/user', {
             headers: {
                 authorization: `Bearer ${accessTokenResponse.access_token}`
             }

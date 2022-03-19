c8d8fd31d5b248646787ae74971621dc118fb81f
ReneSena <rene.sena3005@gmail.com>
2022-03-19 00:56:21 -0300

    Creating a new endpoint to list of last messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLastMessagesController } from "./controllers/GetLastMessagesController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
 
-router.post("/messages", ensureAuthenticated ,new CreateMessageController().handle);
+router.post(
+	"/messages",
+	ensureAuthenticated,
+	new CreateMessageController().handle
+);
+
+router.get("/messages/last-messages", new GetLastMessagesController().handle);
 
 export { router };
